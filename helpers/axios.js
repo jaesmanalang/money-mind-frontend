1c6f21d04ad48604b5c131ae3c43d28cd01256e7
James Manalang <james@MacBook-Air.local>
2023-03-08 01:37:22 +0800

    Add env for api base url

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://money-mind-api-production.up.railway.app/api/v1',
+  // baseURL: 'https://money-mind-api-production.up.railway.app/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: 'https://money-mind-api-production.up.railway.app/api/v1',
+  // baseURL: 'https://money-mind-api-production.up.railway.app/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
 
